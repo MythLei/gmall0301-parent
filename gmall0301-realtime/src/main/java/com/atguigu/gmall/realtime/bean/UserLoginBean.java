d31815139083935d530de0ff660baecb64dd419a
felix <fxf_java@163.com>
2022-08-26 16:42:47 +0800

    回流用户以及加购独立用户统计

---
+++ gmall0301-realtime/src/main/java/com/atguigu/gmall/realtime/bean/UserLoginBean.java
@@ -0,0 +1,25 @@
+package com.atguigu.gmall.realtime.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Felix
+ * @date 2022/8/26
+ * Desc: 用户域用户登录实体类
+ */
+@Data
+@AllArgsConstructor
+public class UserLoginBean {
+    // 窗口起始时间
+    String stt;
+    // 窗口终止时间
+    String edt;
+    // 回流用户数
+    Long backCt;
+    // 独立用户数
+    Long uuCt;
+    // 时间戳
+    Long ts;
+}
+
